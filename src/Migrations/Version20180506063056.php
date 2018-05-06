36cc766f1c1d4c211caaf0e14a19bc248861690a
Yi Wang <y.wang7@uqconnect.edu.au>
2018-05-06 16:32:01 +1000

    alter photo table

---
+++ src/Migrations/Version20180506063056.php
@@ -0,0 +1,28 @@
+<?php declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Migrations\AbstractMigration;
+use Doctrine\DBAL\Schema\Schema;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+class Version20180506063056 extends AbstractMigration
+{
+    public function up(Schema $schema)
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('ALTER TABLE photo DROP owner_id');
+    }
+
+    public function down(Schema $schema)
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('ALTER TABLE photo ADD photo_name LONGTEXT NOT NULL COLLATE utf8mb4_unicode_ci, ADD upload_date DATETIME NOT NULL');
+    }
+}
