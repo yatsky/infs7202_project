4242c9c773585f1d65a550b6fe07cb3ab66c2441
Yi Wang <y.wang7@uqconnect.edu.au>
2018-05-06 16:36:23 +1000

    alter photo table

--- src/Migrations/Version20180506063340.php
+++ src/Migrations/Version20180506063340.php
@@ -8,14 +8,14 @@ use Doctrine\DBAL\Schema\Schema;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-class Version20180506062511 extends AbstractMigration
+class Version20180506063340 extends AbstractMigration
 {
     public function up(Schema $schema)
     {
         // this up() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
-        $this->addSql('ALTER TABLE photo DROP photo_name');
+        $this->addSql('ALTER TABLE photo DROP photo_name, DROP upload_date');
     }
 
     public function down(Schema $schema)
@@ -23,6 +23,6 @@ class Version20180506062511 extends AbstractMigration
         // this down() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
-        $this->addSql('ALTER TABLE photo ADD photo_name LONGTEXT NOT NULL COLLATE utf8mb4_unicode_ci');
+        $this->addSql('ALTER TABLE photo ADD photo_name LONGTEXT NOT NULL COLLATE utf8mb4_unicode_ci, ADD upload_date DATETIME NOT NULL');
     }
 }
