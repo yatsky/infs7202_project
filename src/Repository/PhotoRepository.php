a5f8ce14d07efc7b8b8ea367951423dcb790e6e7
Thomas <wyatsky@gmail.com>
2020-05-10 17:46:33 +1000

    Update Symfony

--- src/Repository/PhotoRepository.php
+++ src/Repository/PhotoRepository.php
@@ -4,7 +4,8 @@ namespace App\Repository;
 
 use App\Entity\Photo;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
-use Symfony\Bridge\Doctrine\RegistryInterface;
+//use Symfony\Bridge\Doctrine\RegistryInterface;
+use Doctrine\Persistence\ManagerRegistry;
 
 /**
  * @method Photo|null find($id, $lockMode = null, $lockVersion = null)
@@ -14,7 +15,7 @@ use Symfony\Bridge\Doctrine\RegistryInterface;
  */
 class PhotoRepository extends ServiceEntityRepository
 {
-    public function __construct(RegistryInterface $registry)
+    public function __construct(ManagerRegistry $registry)
     {
         parent::__construct($registry, Photo::class);
     }
