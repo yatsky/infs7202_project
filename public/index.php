1a0ff307e7fdee1a1ca6f2343e8664fdf0d77f86
Thomas <wyatsky@gmail.com>
2020-05-10 16:33:55 +1000

    Trusting the Heroku Router
    https://devcenter.heroku.com/articles/deploying-symfony4#trusting-the-heroku-router

--- public/index.php
+++ public/index.php
@@ -24,9 +24,18 @@ if ($debug) {
     Debug::enable();
 }
 
+$trustedProxies = $_SERVER['TRUSTED_PROXIES'] ?? $_ENV['TRUSTED_PROXIES'] ?? false;
+$trustedProxies = $trustedProxies ? explode(',', $trustedProxies) : [];
+if($_SERVER['APP_ENV'] == 'prod') $trustedProxies[] = $_SERVER['REMOTE_ADDR'];
+if($trustedProxies) {
+    Request::setTrustedProxies($trustedProxies, Request::HEADER_X_FORWARDED_AWS_ELB);
+}
+
+/*
 if ($trustedProxies = $_SERVER['TRUSTED_PROXIES'] ?? false) {
     Request::setTrustedProxies(explode(',', $trustedProxies), Request::HEADER_X_FORWARDED_ALL ^ Request::HEADER_X_FORWARDED_HOST);
 }
+ */
 
 if ($trustedHosts = $_SERVER['TRUSTED_HOSTS'] ?? false) {
     Request::setTrustedHosts(explode(',', $trustedHosts));
